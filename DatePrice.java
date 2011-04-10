f085bf77fad02d2780f50d357512bc8751a22602
Carnin <jogus09@hotmail.com>
2011-04-10 17:57:57 +0200

    SQL Prototype added. Lib could not be added due to license issues so needs to be downloaded manually from MySQl webpage

---
+++ DatePrice.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+
+public class DatePrice {
+	public Date date;
+	public int price;
+	public DatePrice(Date date, int price){
+		this.date = date;
+		this.price = price;
+	}
+}
---
+++ LoginData.java
@@ -0,0 +1,7 @@
+
+public class LoginData {
+	public static String USER = "developer";
+	public static String PASSWORD = "faroflyg";
+	public static String URL = "jdbc:mysql://www.darknox.net/faroflyg";
+
+}
