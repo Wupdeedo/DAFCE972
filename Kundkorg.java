33e435119480e0677ddee1f3d9c39871f093d784
Wupdeedo <wupdeedo@gmail.com>
2011-04-12 17:57:40 +0200

    fu

--- Kundkorg.java
+++ Kundkorg.java
@@ -1,27 +1,62 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
-public class Kundkorg extends JPanel implements MouseListener{
-	private GeneralPath kop,area;
+public class Kundkorg extends JPanel implements MouseListener, ComponentListener{
+	private GeneralPath kop;
 	private Point2D center;
 	private ArrayList<Slice> slices;
 	private double w,h;
+	private JLabel sumlab, sum;
 	
 	public static void main(String[] args){
 		JFrame jf = new JFrame();
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setBounds(100, 100, 512, 512);
+		jf.setBounds(100, 100, 576, 576);
 		jf.getContentPane().add(new Kundkorg());
 		jf.setVisible(true);
 	}
 	
 	public Kundkorg(){
 		slices = new ArrayList<Slice>();
+		FlygBokning fb1 = new FlygBokning();
+		FlygBokning fb2 = new FlygBokning();
+		FlygBokning fb3 = new FlygBokning();
+		FlygBokning fb4 = new FlygBokning();
+		FlygBokning fb5 = new FlygBokning();
+		slices.add(new Slice(fb1));
+		slices.add(new Slice(fb2));
+		slices.add(new Slice(fb3));
+		slices.add(new Slice(fb4));
+		slices.add(new Slice(fb5));
+		setup();
+		kop = new GeneralPath();
+		Arc2D a = new Arc2D.Double(30,20,515,515,-77.9,47.5,Arc2D.OPEN);
+		kop.moveTo(341,408);
+		kop.append(a, true);
+		
+		this.setLayout(null);
+		
+		sumlab = new JLabel("<html><h2>Summa: </h2></html>");
+		sum = new JLabel("<html><h2>1337 kr</h2></html>");
+		this.add(sum);
+		this.add(sumlab);
+		sumlab.setBounds(150, 450,100,20);
+		sumlab.setForeground(Color.red);
+		sum.setBounds(250, 450,100,20);
+		sum.setForeground(Color.red);
+		
+		repaint();
+		
+		this.addComponentListener(this);
+		this.addMouseListener(this);
 	}
 	
 	public void addBooking(Bokning b){
@@ -32,16 +67,16 @@ public class Kundkorg extends JPanel implements MouseListener{
 	private void setup(){
 		w = this.getWidth();
 		h = this.getHeight();
-		center = new Point2D.Double(w/2.5,h/2);
+		center = new Point2D.Double(w/2,408);
 		
-		double start = -60;
-		double deg = 270;
+		double start = -30.5;
+		double deg = 241;
 		double n = slices.size();
 		deg = deg / n;
 		int i = 1;
 		GeneralPath bounds;
 		
-		Arc2D a = new Arc2D.Double(0,0,w,h,start,deg,Arc2D.OPEN);
+		Arc2D a = new Arc2D.Double(30,20,515,515,start,deg,Arc2D.OPEN);
 		
 		for (Slice s : slices){
 			bounds = new GeneralPath();
@@ -51,16 +86,26 @@ public class Kundkorg extends JPanel implements MouseListener{
 			a.setAngleStart(start+i*deg);
 			i++;
 		}
-		
+		repaint();
 	}
 	
 	public void paint(Graphics graph){
 		Graphics2D g = (Graphics2D) graph;
-		this.paintComponents(graph);
-		for (Slice s : slices){
-			g.draw(s.getBounds());
+		try {
+			g.drawImage(ImageIO.read(new File("src/kundkorg.png")),0,0,null);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+		for (Slice s : slices){
+			Shape b = s.getBounds();
+			g.fill(b);
+			g.setColor(Color.blue);
+			g.draw(b);
+			g.setColor(Color.black);
+		}
+		g.setColor(Color.red);
+		g.draw(kop);
+		this.paintComponents(graph);
 	}
 
 	public void mouseClicked(MouseEvent e) {}
@@ -71,6 +116,14 @@ public class Kundkorg extends JPanel implements MouseListener{
 		
 	}
 	public void mouseReleased(MouseEvent e) {}
+	public void componentResized(ComponentEvent arg0) {
+		setup();
+		invalidate();
+		repaint();
+	}
+	public void componentShown(ComponentEvent arg0) {}
+	public void componentHidden(ComponentEvent arg0) {}
+	public void componentMoved(ComponentEvent arg0) {}
 }
 
 class Slice {
